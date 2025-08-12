691380ba85b6999e28619e1959a967e1ea1a6524
Dennis Carcamos <denniscarcamos@Denniss-MacBook-Pro-2.local>
2025-08-12 15:19:01 -0600

    first page added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,25 @@
 import { NgModule } from '@angular/core';
-import type { Routes } from '@angular/router';
-import { RouterModule } from '@angular/router';
+import { PreloadAllModules, RouterModule, type Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    loadChildren: () =>
+      import('./features/products/products.module').then(
+        // eslint-disable-next-line @typescript-eslint/typedef
+        (m) => m.ProductsModule,
+      ),
+  },
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [
+    RouterModule.forRoot(routes, {
+      useHash: true,
+      scrollPositionRestoration: 'enabled',
+      preloadingStrategy: PreloadAllModules,
+    }),
+  ],
   exports: [RouterModule],
 })
 export class AppRoutingModule {}
---
+++ src/app/features/products/pages/products-list-page/products-list-page.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-products-list-page',
+  templateUrl: './products-list-page.component.html',
+  styleUrls: ['./products-list-page.component.css'],
+})
+export class ProductsListPageComponent implements OnInit {
+  constructor() {
+    // empty
+  }
+
+  public ngOnInit(): void {
+    console.log('Products List Page Initialized');
+  }
+}
